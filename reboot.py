94945853ed9dfc6911c91fd0cc9bcc6f87e718e7
AlbertoRM <albertoarm99@hotmail.com>
2022-09-18 19:31:54 -0500

    seleccion de clientes

--- reboot.py
+++ reboot.py
@@ -1,4 +1,5 @@
-import paramiko
+from urllib import response
+import paramiko,os
 
 def sshReboot(ip):
     username = "BYA"
@@ -6,18 +7,26 @@ def sshReboot(ip):
     cmd = "reboot"
     try:
         ssh = paramiko.SSHClient()
-        
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(ip,22,username,password,timeout=5)
+        ssh.connect(ip,username,password,timeout=5)
         stdin,stdout,stderr = ssh.exec_command(cmd)
-        # stdin.write("Y") #interact with server, typing Y 
         print(stdout.read())
-        # for x in stdout.readlines():
-        # print x.strip("n")
         print('%s OK\n'%(ip))
         ssh.close()
     except :
         print('%s Error\n' %(ip))
+        print(stderr.read())
+
+ipList=open("./lists/client.txt", "r")
+ipList.seek(0)
+
+for hostname in ipList.readlines():
+    response=os.system("ping -n 1 " + hostname)
+    if response == 0:
+        print("Status is up")
+        sshReboot(hostname)
+    else:
+        print("Status is down")
 
 
-ssh2("192.168.55.78")
+#sshReboot("192.168.55.78")
